4d3159adc8c96d4208a1a61715341bf05fb9dc35
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-13 14:17:34 +0300

    refactor: add AuthWrapper

--- src/common/components/authorization/passwordRecovery/PasswordRecovery.tsx
+++ src/common/components/authorization/passwordRecovery/PasswordRecovery.tsx
@@ -2,23 +2,23 @@ import React from "react";
 import s from "./passwordRecovery.module.scss";
 
 import { NavLink } from "react-router-dom";
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import { AuthWrapper } from "common/components/authorization/authWrapper/AuthWrapper";
+import SuperButton from "common/components/superComponents/superButton/SuperButton";
 
 export const PasswordRecovery = () => {
   return (
-    <Paper elevation={2} className={s.login} >
+    <AuthWrapper>
       <h1 className={s.title}>Forgot your password?</h1>
       <div className={s.form}>
         <TextField className={s.input} label="Email" variant="standard" />
         <label>Enter your email address and we will send you further instructions </label>
-        <Button className={s.btn} variant="contained">Contained</Button>
+        <SuperButton className={s.btn}>Send Instructions</SuperButton>
       </div>
       <div className={s.signIn}>
         <p className={s.remember}>Did you remember your password?</p>
         <NavLink className={s.signInBtn} to="/friday-project/login">Try logging in</NavLink>
       </div>
-    </Paper>
+    </AuthWrapper>
   );
 };
