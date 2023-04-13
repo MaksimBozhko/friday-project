ac35a24d51e70bf37d0384f63d55bce2f6f678b3
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-13 15:49:48 +0300

    add: user info in Header

--- src/pages/main/Header/Header.tsx
+++ src/pages/main/Header/Header.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { ReactComponent as Logo } from "common/assets/img/logo.svg";
-import Button from "@mui/material/Button";
 import s from "pages/main/Header/Header.module.scss";
+import SuperButton from "common/components/superComponents/superButton/SuperButton";
+import { User } from "pages/main/Header/user/User";
 
 export const Header = () => {
   return (
     <div className={s.header}>
       <div className={s.container}>
         <Logo />
-        <Button variant="contained">Contained</Button>
+        <SuperButton className={s.btn}>Sign in</SuperButton>
+        {/*<User/>*/}
       </div>
     </div>
   );
