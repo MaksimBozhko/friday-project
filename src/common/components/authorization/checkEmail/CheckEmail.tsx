b5eceb4145dd0df45f8bda99768dbeddebc6ca70
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-13 14:24:00 +0300

    refactor: add AuthWrapper

--- src/common/components/authorization/checkEmail/CheckEmail.tsx
+++ src/common/components/authorization/checkEmail/CheckEmail.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import s from "./checkEmail.module.scss";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
 import { NavLink } from "react-router-dom";
-import {ReactComponent as Email} from "common/assets/img/email.svg";
+import { ReactComponent as Email } from "common/assets/img/email.svg";
+import { AuthWrapper } from "common/components/authorization/authWrapper/AuthWrapper";
+import SuperButton from "common/components/superComponents/superButton/SuperButton";
 
 export const CheckEmail = () => {
   return (
-    <Paper elevation={2} className={s.CheckEmail}>
+    <AuthWrapper>
       <h1 className={s.title}>Check Email</h1>
       <Email />
       <p className={s.text}>We’ve sent an Email with instructions to example@mail.com</p>
-
-        <NavLink className={s.signInBtn} to="/friday-project/login">
-          <Button className={s.btn} variant="contained">Back to login</Button>
-        </NavLink>
-
-    </Paper>
+      <NavLink className={s.loginBtn} to="/friday-project/login">
+        <SuperButton className={s.btn}>Back to login</SuperButton>
+      </NavLink>
+    </AuthWrapper>
   );
 };
