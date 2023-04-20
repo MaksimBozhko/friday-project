a860e942a8354b52c8a3e1a6dd3fa59e87e1cc73
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-20 11:22:26 +0300

    add: newTable

---
+++ src/common/components/tablePacksList/TablePackList.tsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import s from "./table.module.scss"
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import { ReactComponent as Learn } from "common/assets/img/tableIcon/teacher.svg";
+import { ReactComponent as Edit } from "common/assets/img/tableIcon/Edit.svg";
+import { ReactComponent as Delete } from "common/assets/img/tableIcon/Delete.svg";
+import { useSelector } from "react-redux";
+import { AppRootStateType } from "app/store";
+import { useState } from "react";
+import { NavLink, useSearchParams } from "react-router-dom";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0
+  }
+}));
+
+export function TablePackList() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const cardPacks = useSelector((state: AppRootStateType) => state.pack.cardPacks);
+  const myId = useSelector((state: AppRootStateType) => state.auth.id);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };K
+  return (
+    <div className={s.table}>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align="left">Cards</StyledTableCell>
+            <StyledTableCell align="left">Last Updated</StyledTableCell>
+            <StyledTableCell align="left">Created by</StyledTableCell>
+            <StyledTableCell align="left">Actions</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cardPacks.map((pack) => (
+            <StyledTableRow key={pack._id}>
+              <StyledTableCell component="th" scope="row">
+                <NavLink to={`/friday-project/cards/${pack._id}`}>{pack.name}</NavLink>
+              </StyledTableCell>
+              <StyledTableCell align="left">{pack.cardsCount}</StyledTableCell>
+              <StyledTableCell align="left">{pack.updated}</StyledTableCell>
+              <StyledTableCell align="left">{pack.user_name}</StyledTableCell>
+              <StyledTableCell align="left">
+                <Learn />
+                {myId === pack._id && (
+                  <>
+                    <Edit />
+                    <Delete />
+                  </>
+                )}
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[4, 6, 8, 10, 12, 14]}
+        component="div"
+        count={5}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </TableContainer>
+    </div>
+  );
+}
--- src/features/packsList/PacksList.tsx
+++ src/features/packsList/PacksList.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import s from "features/packsList/packsList.module.scss";
 import SuperButton from "common/components/superComponents/superButton/SuperButton";
 import { FilterBlock } from "common/components/filterBlock/FilterBlock";
-import { TablePacksList } from "common/components/tablePacksList/TablePacksList";
-import Pagination from "@mui/material/Pagination";
 import { useSelector } from "react-redux";
 import { AppRootStateType } from "app/store";
 import { Navigate } from "react-router-dom";
 import { useActions } from "common/hooks";
 import { packThunks } from "features/packsList/packsSlice";
+import { TablePackList } from "common/components/tablePacksList/TablePackList";
 
 export const PacksList = () => {
   const isLoggedIn = useSelector((state: AppRootStateType) => state.auth.isLoggedIn);
@@ -27,10 +26,7 @@ export const PacksList = () => {
       <SuperButton className={s.btn} onClick={addPackHandler}>Add new pack</SuperButton>
     </div>
     <FilterBlock />
-    <TablePacksList />
-    <footer>
-      <Pagination count={10} color="primary" />
-    </footer>
+    <TablePackList />
   </>;
 };
 
